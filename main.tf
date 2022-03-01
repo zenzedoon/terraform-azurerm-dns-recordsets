db0c0dae17e6735aef04f485b95524e3dce02e42
Zied BEN SALEM <zbensalem@mantu.com>
2022-03-01 12:38:21 +0100

    update local variables

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "azurerm_dns_mx_record" "this" {
   ttl  = local.mx_recordsets[count.index].ttl
 
   dynamic "record" {
-    for_each = mx_recordsets[count.index].records
+    for_each = local.mx_recordsets[count.index].records
     content {
       preference = split(record.value, " ")[0]
       exchange   = split(record.value, " ")[1]
